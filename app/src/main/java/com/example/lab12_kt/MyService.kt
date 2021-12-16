e01c98598d429742b7f7cad3aa8e861887d27be5
t108360145 <t108360145@ntut.org.tw>
2021-12-16 22:57:56 +0800

    second

--- app/src/main/java/com/example/lab12_kt/MyService.kt
+++ app/src/main/java/com/example/lab12_kt/MyService.kt
@@ -3,20 +3,26 @@ package com.example.lab12_kt
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MyService : Service() {
     override fun onCreate() {
         super.onCreate()
-        Thread{
-            try{
-                Thread.sleep(3000)
-                val intent = Intent(this, SecActivity::class.java)
-                intent.flags=Intent.FLAG_ACTIVITY_NEW_TASK
-                startActivity(intent)
-            }catch (e: InterruptedException){
-                e.printStackTrace()
+        val intent = Intent(this, SecActivity::class.java)
+        runBlocking {
+            launch {
+                try{
+                    delay(3000)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                }catch (e: InterruptedException){
+                    e.printStackTrace()
+                }
             }
-        }.start()
+        }
+
     }
 
     override fun onStartCommand(intent: Intent,
